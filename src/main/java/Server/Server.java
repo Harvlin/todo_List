61d9d122fd0c1f7e3dadd1c11f1e9182beb7dac8
Harvlin <harvlinmaximillian@gmail.com>
2024-08-26 23:38:39 +0700

    Commit

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -15,7 +15,7 @@ public class Server {
     private static ExecutorService threadPool = Executors.newCachedThreadPool();
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 Socket socket = serverSocket.accept();
                 threadPool.submit(new ClientHandler(socket));
@@ -26,86 +26,99 @@ public class Server {
     }
 
     private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
+        Socket socket;
         private String nickname;
 
         public ClientHandler(Socket socket) {
-            this.clientSocket = socket;
+            this.socket = socket;
         }
 
         @Override
         public void run() {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                      Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
 
-                    if (!authenticateUser(connection, in, out)) {
+                    if (!authenticateUser(in, out, connection)) {
                         return;
                     }
 
                     String request;
                     while ((request = in.readLine()) != null) {
-                        System.out.println("Received: " + request);
-                        String response = handleRequest(request, connection);
-                        out.println(response);
-                        out.flush();
+                        System.out.println("Received" + request);
+                        String response = requestHandler(request, connection);
+                        System.out.println(response);
                     }
                 }
-            } catch (SQLException | IOException | ClassNotFoundException e) {
-                e.printStackTrace();
+            } catch (SQLException | ClassNotFoundException | IOException e) {
+                System.out.println(e.getMessage());
             } finally {
                 try {
-                    clientSocket.close();
+                    socket.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.out.println(e.getMessage());
                 }
             }
         }
 
-        private String handleRequest(String request, Connection connection) {
+        private String requestHandler(String request, Connection connection) throws SQLException {
             String[] parts = request.split(" ", 2);
             String command = parts[0].toLowerCase();
             String argument = parts.length > 1 ? parts[1] : "";
 
-            try {
-                switch (command) {
-                    case "add":
-                        String[] parameters = argument.split(", ", 3);
-                        return addTask(connection, Integer.parseInt(parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
-                    case "list":
-                        return listTask(connection);
-                    case "completed":
-                        return setCompleted(connection, Integer.parseInt(argument));
-                    case "delete":
-                        return deleteTask(connection, Integer.parseInt(argument));
-                    default:
-                        return "Invalid command or parameters";
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Error";
+            switch (command) {
+                case "add":
+                    String[] parameters = argument.split(", ", 2);
+                    return addTask(connection, nickname, parameters[0], parameters[1]);
+                case "list":
+                    return listTask(connection);
+                case "completed":
+                    return setComplete(connection, argument);
+                case "delete":
+                    return deleteTask(connection, argument);
+                default:
+                    return "Invalid";
             }
         }
 
-        private String setCompleted(Connection connection, int ID) throws SQLException {
-            String query = "UPDATE tasks SET completed = 1 WHERE id = ? AND completed = 0";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setInt(1, ID);
-                preparedStatement.executeUpdate();
-                return "Task Completed";
+        private String deleteTask(Connection connection, String identifier) throws SQLException{
+            String query;
+            PreparedStatement preparedStatement;
+
+            if (isNumeric(identifier) /* Return true if identifier is String */) {
+                query = "DELETE FROM tasks WHERE id = ? AND owner = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1, Integer.parseInt(identifier));
+            } else {
+                query = "DELETE FROM tasks WHERE title = ? AND owner = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, identifier);
             }
+            preparedStatement.setString(2, nickname);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            return rowsAffected > 0 ? "Deleted" : "Not found";
         }
 
-        private String deleteTask(Connection connection, int ID) throws SQLException {
-            String query = "DELETE FROM tasks WHERE id = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setInt(1, ID);
-                preparedStatement.executeUpdate();
-                return "Deleted";
+        private String setComplete(Connection connection, String identifier) throws SQLException{
+            String query;
+            PreparedStatement preparedStatement;
+
+            if (isNumeric(identifier) /* Return true if identifier is String */) {
+                query = "UPDATE tasks SET completed = 1 WHERE id = ? AND owner = ? AND completed = 0";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1, Integer.parseInt(identifier));
+            } else {
+                query = "UPDATE tasks SET completed = 1 WHERE title = ? AND owner = ? AND completed = 0";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, identifier);
             }
+            preparedStatement.setString(2, nickname);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            return rowsAffected > 0 ? "Completed" : "Not found or Already Completed";
         }
 
         private String listTask(Connection connection) throws SQLException {
@@ -115,6 +128,7 @@ public class Server {
                 ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
                     response.append("ID: ").append(resultSet.getInt("id"))
+                            .append(", Title: ").append(resultSet.getString("title"))
                             .append(", Task: ").append(resultSet.getString("task"))
                             .append(", Created at: ").append(resultSet.getTimestamp("created_at"))
                             .append(", Status: ").append(resultSet.getBoolean("completed") ? "Done" : "Not completed")
@@ -124,52 +138,45 @@ public class Server {
             return response.toString();
         }
 
-        private String addTask(Connection connection, int userId, String task, int status) throws SQLException {
-            String query = "INSERT INTO tasks (user_id, task, completed) VALUES (?, ?, ?)";
+        private String addTask(Connection connection, String owner, String title, String task) throws SQLException {
+            String query = "INSERT INTO tasks (owner, title, task) VALUES (?, ?, ?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setInt(1, userId);
-                preparedStatement.setString(2, task);
-                preparedStatement.setInt(3, status);
+                preparedStatement.setString(1, owner);
+                preparedStatement.setString(2, title);
+                preparedStatement.setString(3, task);
                 preparedStatement.executeUpdate();
                 return "Task added";
             }
         }
 
-        private boolean authenticateUser(Connection connection, BufferedReader in, PrintWriter out) throws SQLException, IOException {
+        private boolean authenticateUser(BufferedReader in, PrintWriter out, Connection connection) throws IOException, SQLException {
             out.println("Nickname: ");
-            out.flush();
             nickname = in.readLine();
 
-            String nickQuery = "SELECT * FROM users WHERE username = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(nickQuery)) {
+            String query = "SELECT * FROM users WHERE username = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
                 preparedStatement.setString(1, nickname);
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     if (!resultSet.next()) {
-                        out.println("User doesn't exist. Please register.");
-                        out.flush();
-                        registerUser(connection, in, out);
-                        out.println("You can now log in.");
-                        out.flush();
+                        out.println("User doesn't exist");
+                        registerUser(in, out, connection);
+                        out.println("You can now log in");
                         return false;
                     } else {
-                        out.println("Enter your password: ");
-                        out.flush();
+                        out.println("Enter your password");
                         String password = in.readLine();
                         if (!password.equals(resultSet.getString("password"))) {
                             out.println("Wrong password");
-                            out.flush();
                             return false;
-                        } else {
-                            return true;
                         }
+                        return true;
                     }
                 }
             }
         }
 
-        private void registerUser(Connection connection, BufferedReader in, PrintWriter out) throws SQLException, IOException {
-            out.println("Enter a password: ");
-            out.flush();
+        private void registerUser(BufferedReader in, PrintWriter out, Connection connection) throws SQLException, IOException{
+            out.println("Enter a password");
             String password = in.readLine();
 
             String query = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -177,6 +184,15 @@ public class Server {
                 preparedStatement.setString(1, nickname);
                 preparedStatement.setString(2, password);
                 preparedStatement.executeUpdate();
+            }
+        }
+
+        private boolean isNumeric(String txt) {
+            try {
+                Integer.parseInt(txt);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
             }
         }
     }
